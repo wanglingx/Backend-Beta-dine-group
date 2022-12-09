d0a94e5820e97bc3527b7ff8372d863f93b7187e
Jialing Wang <annsanst62@gmail.com>
2022-12-09 21:20:23 +0700

    clean

--- routes/router.js
+++ routes/router.js
@@ -4,6 +4,7 @@ const { EndpointSearch } = require('../src/service/search/endpoint');
 const { EndpointScore } = require('../src/service/score/endpoint');
 const { EndpointRank } = require('../src/service/rank/endpoint');
 const { EndpointSelect } = require('../src/service/select/endpoint');
+
 //search router
 router.get("/search/:keyword", new EndpointSearch().searchEndpoint)
 router.get("/seachbyReligion/:religion", new EndpointSearch().searchbyReligionEndpoint)
@@ -19,7 +20,6 @@ router.get("/filterSearchByCanteenOnly/:canteen_id", new EndpointSearch().filter
 router.get("/filterSearchByTypeOnly/:type_id", new EndpointSearch().filterSearchByTypeOnlyEndpoint)
 router.get("/filterSearchByReligionOnly/:religion_id", new EndpointSearch().filterSearchByReligionOnlyEndpoint)
 
-
 //score router
 router.get("/getUser/:student_id", new EndpointScore().getUserEndpoint);
 router.get('/addScoreFood/:menu_id', new EndpointScore().addScoreEndpoint)
@@ -33,6 +33,7 @@ router.get("/addMenuScore/:menu_id", new EndpointRank().addMenuScoreEndpoint);
 router.get("/getMenuInfo/:menu_id", new EndpointRank().getMenuInfoEndpoint);
 router.get("/addRestaurantScore/:menu_id", new EndpointRank().addRestaurantScoreEndpoint);
 router.get("/getAvgRestaurant/:restaurant_id", new EndpointRank().getAvgRestaurantEndpoint);
+
 //select router
 router.get("/RetuarantInfo/restuarantid=:restuarant_id",new EndpointSelect().getRestuarantInfoEndpoint)
 router.get("/MenuInfo/menuid=:menu_id", new EndpointSelect().getMenuInfoEndpoint)
